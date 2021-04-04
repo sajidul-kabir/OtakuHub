96769bfed22ad60d5b544197388ed2891a380a1a
Siam Ahmed <52734646+siam397@users.noreply.github.com>
2021-04-04 22:55:38 +0600

    communities connected with db

--- src/Containers/HomePage/HomePage.js
+++ src/Containers/HomePage/HomePage.js
@@ -2,19 +2,29 @@ import React,{Component} from 'react'
 import {connect} from 'react-redux'
 import classes from './Homepage.css'
 import Community from '../../Components/community/community'
+import * as actionCreators from '../../Store/actions/communityActions'
 class HomePage extends Component{
+
+    componentDidMount(){
+        this.props.getCommunities()
+    }
+
+    cardClickHandler=(name)=>{
+        console.log(name);
+        const link="/"+encodeURIComponent(name)+"/posts"
+        this.props.history.push(link)
+    }
+
     render(){
 
         const communities=this.props.communities.map(community=>{
             return(
                 <div>
-                    <Community name={community.name} author={community.author} members={community.members}/>
+                    <Community name={community.name} clicked={this.cardClickHandler.bind(this,community.name)} image={community.images[0]} author={community.author} members={community.members}/>
                 </div>
             )
         })
 
-
-        console.log(this.props.communities);
         return(
             <div className={classes.communitiesMainDiv}>
                 {communities}
@@ -32,7 +42,7 @@ const mapPropsToState=(state)=>{
 
 const mapDispatchToProps=(dispatch=>{
     return{
-        AddNewCommunity:()=>dispatch({type:"add"})
+        getCommunities:()=>dispatch(actionCreators.getAllCommunities())
     }
 })
 
